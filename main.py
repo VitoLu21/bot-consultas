7dc69b631ad790579c9f7de352565987968c49dc
Víctor Adrián Lucero <victoradrianlucero@MacBook-Pro-de-Ayo.local>
2025-04-17 16:45:21 -0300

    Primera versión del backend

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request, HTTPException
+from pydantic import BaseModel
+from datetime import datetime
+
+app = FastAPI()
+
+consultas = []
+CLAVE_SECRETA = "UNIVERSIDAD2025"
+
+@app.middleware("http")
+async def verificar_token(request: Request, call_next):
+    token = request.headers.get("Authorization")
+    if token != f"Bearer {CLAVE_SECRETA}":
+        raise HTTPException(status_code=403, detail="No autorizado")
+    return await call_next(request)
+
+class Consulta(BaseModel):
+    usuario: str
+    mensaje: str
+
+@app.post("/guardar_consulta")
+async def guardar_consulta(data: Consulta):
+    consultas.append({
+        "usuario": data.usuario,
+        "mensaje": data.mensaje,
+        "fecha": datetime.now().isoformat()
+    })
+    return {"estado": "ok", "mensaje_guardado": data.mensaje}
